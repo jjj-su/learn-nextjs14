5d466fcfe1931190ee1be87033cbebf2aad75a66
jisu <ssu7358@naver.com>
2024-07-31 17:29:08 +0900

    [fix] build error fix

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,5 +1,5 @@
+import { API_URL } from "../app/constant";
 import styles from "../styles/movie-videos.module.css";
-import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
   const response = await fetch(`${API_URL}/${id}/videos`);
